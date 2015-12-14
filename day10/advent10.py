bcc087bd4ef6458f29cc64c15e9c8c0e020522cc
Daniel Mendelsohn <daniel.l.mendelsohn@gmail.com>
2015-12-13 20:30:15 -0500

    day 10

--- day10/advent10.py
+++ day10/advent10.py
@@ -1,13 +1,34 @@
-def part1Answer(f):
-	return 0
+START = "1113222113"
 
-def part2Answer(f):
-	return 0
+# num is a string like "1112"
+def lookAndSay(num):
+	newNum = ""
+	lastDigit = num[0]
+	runCount = 0
+	for digit in num:
+		if digit == lastDigit:
+			runCount += 1
+		else:
+			newNum += str(runCount) + lastDigit
+			lastDigit = digit
+			runCount = 1
+	newNum += str(runCount) + lastDigit
+	return newNum
+
+def iterLookAndSay(start, numTimes):
+	num = start
+	for i in range(numTimes):
+		num = lookAndSay(num)
+	return num
+
+def part1Answer():
+	return len(iterLookAndSay(START, 40))
+
+def part2Answer():
+	return len(iterLookAndSay(START, 50))
 
 
 if __name__ == "__main__":
-	f = open('input.txt', 'rt')
-	print("Part 1: %d" % (part1Answer(f),))
-	f.seek(0)
-	print("Part 2: %d" % (part2Answer(f),))
+	print("Part 1: %d" % (part1Answer(),))
+	print("Part 2: %d" % (part2Answer(),))
 
