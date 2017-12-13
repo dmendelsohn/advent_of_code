f0e710c0dafc0e963b40e6ed0146b7742d057544
Daniel Mendelsohn <daniel.l.mendelsohn@gmail.com>
2017-12-13 01:04:51 -0500

    Finished day 9

--- 2017/day09/day09.py
+++ 2017/day09/day09.py
@@ -1,12 +1,41 @@
-def part1Answer(f):
-    return 0
+def score(line):
+    score_so_far = 0
+    garbage_count = 0
+    num_groups_open = 0
+    is_garbage = False
+    is_cancel = False
+    for c in line:
+        # Skip char if cancelled
+        if is_cancel:
+            is_cancel = False
+            continue
 
-def part2Answer(f):
-    return 0
+        if is_garbage and c not in ['!', '>']:
+            garbage_count += 1
+
+        if c == '{':
+            if not is_garbage:
+                num_groups_open += 1
+                score_so_far += num_groups_open
+        elif c == '}':
+            if not is_garbage:
+                num_groups_open -= 1
+        elif c == '<':
+            is_garbage = True
+        elif c == '>':
+            is_garbage = False
+        elif c == '!':
+            is_cancel = True
+
+    return score_so_far, garbage_count
+
+def answer(f, part):
+    lines = f.read().strip().split('\n')
+    return sum(score(line)[part-1] for line in lines)
 
 if __name__ == "__main__":
     f = open('input.txt', 'rt')
-    print("Part 1: %d" % (part1Answer(f),))
+    print("Part 1: %d" % (answer(f, 1),))
     f.seek(0)
-    print("Part 2: %d" % (part2Answer(f),))
+    print("Part 2: %d" % (answer(f, 2),))
 
