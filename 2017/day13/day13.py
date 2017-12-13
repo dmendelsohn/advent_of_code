3eebef4c80eb2faed3e89fc3fe2c88c624e77d31
Daniel Mendelsohn <daniel.l.mendelsohn@gmail.com>
2017-12-13 10:07:19 -0500

    Day 13

--- 2017/day13/day13.py
+++ 2017/day13/day13.py
@@ -1,8 +1,31 @@
+def parse_input(f):
+    lines = f.read().strip().split('\n')
+    firewall = {}
+    for line in lines:
+        line = line.replace(':', '')
+        parts = line.split(' ')
+        firewall[int(parts[0])] = int(parts[1])
+    return firewall
+
+def get_cost(firewall, start=0, penalize_row_0=False):
+    cost = 0
+    for (row, depth) in firewall.items():
+        if (start+row)%(2*(depth-1)) == 0:
+            cost += row*depth
+    if penalize_row_0 and start%(2*(firewall[0]-1)) == 0:
+        cost += 1
+    return cost
+
 def part1Answer(f):
-    return 0
+    firewall = parse_input(f)
+    return get_cost(firewall)
 
 def part2Answer(f):
-    return 0
+    firewall = parse_input(f)
+    start = 0
+    while get_cost(firewall, start, True) != 0:
+        start += 1
+    return start
 
 if __name__ == "__main__":
     f = open('input.txt', 'rt')
