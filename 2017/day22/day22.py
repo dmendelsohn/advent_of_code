1a9850e8ed290169c14b24a7e656c57ff4156b27
Daniel Mendelsohn <daniel.l.mendelsohn@gmail.com>
2017-12-20 01:10:36 -0500

    Templates for final 6 days

---
+++ 2017/day22/day22.py
@@ -0,0 +1,12 @@
+def part1Answer(f):
+    return 0
+
+def part2Answer(f):
+    return 0
+
+if __name__ == "__main__":
+    f = open('input.txt', 'rt')
+    print("Part 1: {}".format(part1Answer(f)))
+    f.seek(0)
+    print("Part 2: {}".format(part2Answer(f)))
+
