ac98ae6dec483fe07547d19f1151c377fc25d536
Daniel Mendelsohn <daniel.l.mendelsohn@gmail.com>
2023-09-03 00:14:51 -0400

    2022 Day 25 in Python

--- python/src/year2022/day25/solution.py
+++ python/src/year2022/day25/solution.py
@@ -1,6 +1,34 @@
+def decode_digit(digit: str) -> int:
+    return {"0": 0, "1": 1, "2": 2, "-": -1, "=": -2}[digit]
+
+
+def decode(snafu: str) -> int:
+    """Decode SNAFU number"""
+    place = 1
+    total = 0
+    for digit in reversed(snafu):
+        total += place * decode_digit(digit)
+        place *= 5
+    return total
+
+
+def encode_digit(num: int) -> str:
+    return {0: "0", 1: "1", 2: "2", -1: "-", -2: "="}[num]
+
+
+def encode(num: int) -> str:
+    """Encode SNAFU number"""
+    digits = []  # Encoded digits (in reverse)
+    while num:
+        lowest_digit_value = ((num + 2) % 5) - 2
+        digits.append(encode_digit(lowest_digit_value))
+        num = (num - lowest_digit_value) // 5
+    return "".join(reversed(digits))
+
+
 def part_1(puzzle_input: str) -> str | int:
-    return "Not implemented"
+    return encode(sum(decode(line) for line in puzzle_input.split("\n")))
 
 
 def part_2(puzzle_input: str) -> str | int:
-    return "Not implemented"
+    return "N/A - freebie"
