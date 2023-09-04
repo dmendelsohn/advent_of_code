87e14fe6fd89d69f3718317c0ec27f13a58613b2
Daniel Mendelsohn <daniel.l.mendelsohn@gmail.com>
2023-09-04 18:32:55 -0400

    2022 Day 6 in Python

--- python/src/year2022/day06/solution.py
+++ python/src/year2022/day06/solution.py
@@ -1,6 +1,10 @@
+def find_marker(s: str, n: int) -> int:
+    return next(idx for idx in range(n, len(s)) if len(set(s[idx - n : idx])) == n)
+
+
 def part_1(puzzle_input: str) -> str | int:
-    return "Not implemented"
+    return find_marker(puzzle_input, 4)
 
 
 def part_2(puzzle_input: str) -> str | int:
-    return "Not implemented"
+    return find_marker(puzzle_input, 14)
